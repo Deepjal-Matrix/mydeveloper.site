d530749222570f6bd1ba847bce70b2bb297d4882
Deepjal-Matrix <127577880+Deepjal-Matrix@users.noreply.github.com>
2023-03-25 11:22:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+    });
+
+    
+  $('.accordion-header').click(function(){
+    $('.accordion .accordion-body').slideUp();
+    $(this).next('.accordion-body').slideDown();
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');
+  });
+});
